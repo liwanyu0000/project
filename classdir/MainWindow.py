3985837f9d1f2611e33dd0b7a6ebf170c31b868e
liwanyu0000 <liwanyu0000@outlook.com>
2023-03-21 15:04:26 +0800

    liwanyu

--- classdir/MainWindow.py
+++ classdir/MainWindow.py
@@ -1,42 +1,68 @@
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QProgressBar
 from classdir.Ui_Window import Ui_MainWindow
-# from classdir.Query import Ui_QueryUI
 from classdir.Worker import DetectDirThread
 from classdir.Yolo import YOLO
-import os
+import pathlib
 
 class MainWindow(QMainWindow):
+    # 识别结果存放目录
     xmlPath = 'xmlpath/'
-    _imageFolderPath = "C:/Users"
+    # 支持的图像格式列表
+    includedExtensions = ['jpg', 'jpeg', 'bmp', 'png', 'dib', 'jpe', 'pbm', 'pgm', 'ppm', 'tiff', 'tif']
+    # 文件夹目录
+    imageFolderPath = str(pathlib.Path.home())
+    # yolo输入图像尺寸
+    imageShape = [832, 608]
+    # 瑕疵类别字典
+    classesDict = {0 : 'edge anomaly',
+                   1 : 'corner anomaly',
+                   2 : 'white point blemishes',
+                   3 : 'light block blemishes',
+                   4 : 'dark spot blemishes',
+                   5 : 'aperture blemishes'}
+    
     def __init__(self):
         super(MainWindow, self).__init__()
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
-        self._ui.pushButton.clicked.connect(self.showChild)
-        self._ui.fileButton.clicked.connect(self.openFolder)
+        
+        # 设置状态栏
         self.statusBar().showMessage("就绪！")
-        self._ui.tabWidget.tabBar().hide()
         self.progressBar = QProgressBar(self, textVisible=True)
         self.progressBar.setStyleSheet("QProgressBar { border: 2px solid grey; border-radius: 5px; color: rgb(0, 0, 0);  \
                                         background-color: #FFFFFF; text-align: center;} \
                                         QProgressBar::chunk {background-color: rgb(0, 255, 0); \
                                         border-radius: 10px; margin: 0.1px;  width: 1px;}")
-        self.progressBar.resize(300, 20)
         self.statusBar().addPermanentWidget(self.progressBar)
-        self.imageShape = [832, 608]
+        
+        # 设置隐藏QTabWidget的标签
+        self._ui.tabWidget.tabBar().hide()
+        
+        # 创建yolo模型
         self.model = YOLO(self.imageShape)
+        
+        # 注册
+        self._ui.pushButton.clicked.connect(self.showChild)
+        self._ui.fileButton.clicked.connect(self.openFolder)
+        
     def showChild(self):
         # self.show()
         self._ui.tabWidget.setCurrentIndex(1)
-        
+    
+    # 选择文件夹    
     def openFolder(self):
-        self._imageFolderPath = QFileDialog.getExistingDirectory(
-            self, "选择文件夹", self._imageFolderPath) + "/"
-        self.startDetectDir()
-        
+        try:
+            self.imageFolderPath = QFileDialog.getExistingDirectory(
+                self, "选择文件夹", self.imageFolderPath) + "/"
+            if (self.imageFolderPath != '/'):
+                self.startDetectDir()
+            else:
+                self.imageFolderPath = str(pathlib.Path.home())
+        except Exception as r:
+            print('未知错误 %s' %(r))
+    
+    # 识别文件夹   
     def startDetectDir(self):
-        self.thread_1 = DetectDirThread(self._imageFolderPath, self)
+        self.thread_1 = DetectDirThread(self)
         self.thread_1.start()
-        
-
         
--- classdir/Worker.py
+++ classdir/Worker.py
@@ -3,25 +3,20 @@ import os
 from utils.utilsDetect import detectImage
 
 class DetectDirThread(QThread):
-    def __init__(self, imageDir, window) -> None:
+    def __init__(self, window) -> None:
         super().__init__()
-        self._imageDir = imageDir
-        self._imageList = os.listdir(imageDir)
         self._window = window
+        self._imageList = [fileName for fileName in os.listdir(self._window.imageFolderPath)
+                           if any(fileName.endswith(extension) for extension in self._window.includedExtensions)]
     def run(self):
         self._window.statusBar().showMessage("正在识别(文件夹)...")
         self._window.progressBar.setRange(0, len(self._imageList) - 1)
         self._window.progressBar.reset()
         for step, fileName in enumerate(self._imageList):
-            detectImage(self._imageDir, fileName, self._window.imageShape,
+            print(step + 1, " :", fileName)
+            detectImage(self._window.imageFolderPath, fileName, self._window.imageShape,
                         self._window.model, self._window.xmlPath,
-                        {
-                            0 : 'edge_anomaly',
-                            1 : 'corner_anomaly',
-                            2 : 'white_point_blemishes',
-                            3 : 'light_block_blemishes',
-                            4 : 'dark_spot_blemishes',
-                            5 : 'aperture_blemishes'
-                        })
+                        self._window.classesDict)
             self._window.progressBar.setValue(step)
         self._window.statusBar().showMessage("就绪！")
+        
